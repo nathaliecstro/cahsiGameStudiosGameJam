37117c3dfa1748e56b0e220f8b67a13286033b51
Nathalie Castro <nathalie.castro@upr.edu>
2025-05-02 14:54:26 -0400

    completed game ui and scene transitions

--- cgs_game_jam_bullseye/levels/final_scene.gd
+++ cgs_game_jam_bullseye/levels/final_scene.gd
@@ -1,6 +1,5 @@
 extends Node
 
-const FILE_BEGIN = "res://levels/"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,6 +7,5 @@ func _ready():
 	
 	await get_tree().create_timer(18.5).timeout
 	
-	var next_level_path = FILE_BEGIN + "level_1.tscn"
-	get_tree().change_scene_to_file(next_level_path)
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn") 
 		
