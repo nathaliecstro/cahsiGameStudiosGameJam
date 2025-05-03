eed323f33de4625976b089d0025e028c4f34e18d
Nathalie Castro <nathalie.castro@upr.edu>
2025-05-03 19:02:11 -0400

    implemented game instructions scene

--- cgs_game_jam_bullseye/scenes/game_instructions.gd
+++ cgs_game_jam_bullseye/scenes/game_instructions.gd
@@ -1,20 +1,17 @@
 extends Control
 
 
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func _ready():
+	$wasd.play()
+	$spaceBar.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/gameInstructions.tscn")
-
-
-func _on_quit_pressed():
-	get_tree().quit()
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+	$wasd.stop()
+	$spaceBar.stop()
